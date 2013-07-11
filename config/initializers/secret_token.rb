09bf43dd8ceaf7025906781e10f455a221b4fc06
Alice Lara <hello@alicelara.ca>
2013-07-11 14:19:08 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amazonia::Application.config.secret_token = 'd39e227f8f4084cf52be03d4021b9baf4bb4a25c11984c5dfa1d25615d800151b9019258b0ad0949aaf5aea564e6fbada8a41b4f957c197fda815ebd095d63be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Amazonia::Application.config.session_store :cookie_store, key: '_amazonia_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Amazonia::Application.config.session_store :active_record_store
