7819a506200e1a2bc410a68b458710c33c9ed89b
Alice Lara <hello@alicelara.ca>
2013-07-11 15:27:39 -0400

    generating product controller ans views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,45 @@
+class ProductsController < ApplicationController
+  def index
+  	@products = Product.all
+  end
+
+  def show
+  	@product = Product.find(params[:id])
+  end
+
+  def new
+  	@product = Product.new
+  end
+
+  def create
+  	@product = Product.new(params[:product])
+
+  	if @product.save
+  		redirect_to @product
+  	else
+  		render :new
+  end
+
+  def edit
+  	@product = Product.find(params[:id])
+  end
+
+	def update
+  	@product = Product.find(params[:id])
+
+  	if @product.update_attributes(params[:product])
+  		redirect_to @product
+  	else
+  		render :edit
+  		
+  	end
+  end
+
+  def destroy
+  	@product = Product.find(params[:id])
+		@product.destroy	
+  	end
+  end
+
+
+end
