f814f14fe8198dfb3f5728cb4f47a77645df2db2
Alice Lara <hello@alicelara.ca>
2013-07-14 19:51:08 -0400

    validating password in the user model, including routes for users and generating user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password_digest
+
+  has_secure_password
+  validates_presence_of :password, :on => create
+  validates_presence_of :email, :on => create
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Amazonia::Application.routes.draw do
+  get "users/new"
+
+  get "users/create"
+
  root :to => "products#index"
 
  resources :products
